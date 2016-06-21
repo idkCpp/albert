e474677527b0456c0a4087e365b7cad7af710045
idkCpp <martin.buergmann@gmail.com>
2016-06-21 15:26:20 +0200

    Added basic Remmina-Plugin with skeleton-functionality
    Does not actually work

---
+++ src/plugins/remmina/extension.cpp
@@ -0,0 +1,102 @@
+// albert - a simple application launcher for linux
+// Copyright (C) 2014-2015 Manuel Schneider
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <QDebug>
+#include <QFile>
+#include <QDirIterator>
+#include <QSettings>
+#include "extension.h"
+#include "configwidget.h"
+#include "item.h"
+#include "query.h"
+#include "objects.hpp"
+#include "xdgiconlookup.h"
+
+/** ***************************************************************************/
+Remmina::Extension::Extension() : IExtension("Remmina") {
+    qDebug("[%s] Initialize extension", name_);
+    // Do sth.
+    qDebug("[%s] Extension initialized", name_);
+}
+
+
+
+/** ***************************************************************************/
+Remmina::Extension::~Extension() {
+    qDebug("[%s] Finalize extension", name_);
+    // Do sth.
+    qDebug("[%s] Extension finalized", name_);
+}
+
+
+
+/** ***************************************************************************/
+QWidget *Remmina::Extension::widget(QWidget *parent) {
+    if (widget_.isNull()) {
+        widget_ = new ConfigWidget(parent);
+    }
+    return widget_;
+}
+
+
+
+/** ***************************************************************************/
+void Remmina::Extension::setupSession() {
+    for (Item* item : availableItems_)
+        delete item;
+    availableItems_.clear();
+
+    QDir remminaDir = QDir::home();
+    remminaDir.cd(".remmina");
+    QDirIterator it(remminaDir);
+    while (it.hasNext()) {
+        it.next();
+        if (it.fileName() == "." || it.fileName() == "..")
+            continue;
+
+        QFile nextFile(it.filePath());
+        if (nextFile.open(QFile::ReadOnly)) {
+
+            char* firstBytes = new char[10];
+            nextFile.read(firstBytes, 9);
+            firstBytes[9] = 0;
+            QString signature(firstBytes);
+            delete firstBytes;
+            if (signature != "[remmina]") {
+                qDebug("[%s] Ignoring file %s", name_, it.fileName().toStdString().c_str());
+                continue;
+            }
+
+            nextFile.close();
+            availableItems_.append(new Item(it.filePath()));
+
+        } else
+            qWarning("[%s] Could not read file %s", name_, it.fileName().toStdString().c_str());
+    }
+}
+
+
+
+/** ***************************************************************************/
+void Remmina::Extension::handleQuery(shared_ptr<Query> query) {
+    if (availableItems_.size() == 0) {
+        query->addMatch(std::shared_ptr<AlbertItem>(new StandardItem("No config entry found", "", XdgIconLookup::instance()->themeIconPath("dialog-error", QIcon::themeName()), [](){})));
+        return;
+    }
+    for (Item* item : availableItems_) {
+        query->addMatch(std::shared_ptr<AlbertItem>(item->copy()));
+    }
+}
---
+++ src/plugins/remmina/item.cpp
@@ -0,0 +1,72 @@
+// albert - a simple application launcher for linux
+// Copyright (C) 2014-2015 Manuel Schneider
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "item.h"
+#include <QSettings>
+
+
+/** ***************************************************************************/
+Remmina::Item::Item(QString configFilePath){
+    configFile_ = configFilePath;
+    QSettings set(configFilePath, QSettings::IniFormat);
+    set.beginGroup("remmina");
+    name_ = set.value("name").toString();
+}
+
+
+
+/** ***************************************************************************/
+Remmina::Item::~Item(){
+
+}
+
+
+
+/** ***************************************************************************/
+Remmina::Item *Remmina::Item::copy()
+{
+    return new Item(configFile_);
+}
+
+
+
+/** ***************************************************************************/
+QString Remmina::Item::text() const {
+    return name_;
+}
+
+
+
+/** ***************************************************************************/
+QString Remmina::Item::subtext() const {
+    return "Connect with remmina";
+}
+
+
+
+/** ***************************************************************************/
+QString Remmina::Item::iconPath() const {
+    // Icon of item
+    return QString();
+}
+
+
+
+/** ***************************************************************************/
+void Remmina::Item::activate(ExecutionFlags *) {
+    // Do sth cool...
+    qDebug("Connecting to %s", name_.toStdString().c_str());
+}
